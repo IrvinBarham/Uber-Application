e2a4f9df0076da90962d906d3db25dfa9f2ca1b8
IrvinBarham <70603118+IrvinBarham@users.noreply.github.com>
2021-05-06 18:44:39 -0500

    Add files via upload

--- LoginPage.php
+++ LoginPage.php
@@ -81,42 +81,6 @@ function test_input($data) {
 		<label for="key"><b>Password</b></label>
 		<input type="password" id="key" name="key" required>
 		<input type="submit" value="Submit">
-
-<script>
-	function validateSignIn() {
-		//List of Users
-		let userIDdata = ['user1'];
-		let driverIDdata = ['driver1', 'driver2'];
-		let AdminIDdata = ['boss'];
-		let userPASSdata = ['1'];
-        let driverPASSdata = ['2', '3'];
-		let adminPASSdata = ['4'];
-
-        var id = document.getElementById("userID").value;
-		var pass = document.getElementByID("key").value;
-
-		flagID = false;
-		flagPass = false;
-		
-		for (i = 0; i < userIDdata.length; i++) {
-			if (id.value.match(userIDdata[i])
-				flagID = true;
-		}
-		for (i = 0; i < userPASSdata.length; i++){
-			if (id.value.match(userPassdata[i])
-				flagPass = true;
-		}
-		if (flagID = 'true' && flagPass = 'true') {
-			alert("Successful");
-			return true;
-			}
-		else {
-			alert("Incorrect Login");
-			return false;
-			}
-	};
-
-</script>
 </form>
 		<hr>
 		<h3> Not A Member? </h3>
